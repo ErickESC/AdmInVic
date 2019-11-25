512f9cac3eec80b71b79a60334229b1a160aecfa
erick <erick@jarvis>
2019-11-25 03:34:55 -0600

    ultimo Commit

---
+++ ProyectoAYDAdminVic/src/test/mx/uam/ayd/proyecto/datos/DAOArticuloEnAlmacenBDTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package test.mx.uam.ayd.proyecto.datos;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author erick
+ *
+ */
+public class DAOArticuloEnAlmacenBDTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link mx.uam.ayd.proyecto.datos.DAOArticuloEnAlmacenBD#crea(mx.uam.ayd.proyecto.negocio.dominio.ArticuloEnAlmacen)}.
+	 */
+	@Test
+	public void testCrea() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link mx.uam.ayd.proyecto.datos.DAOArticuloEnAlmacenBD#recupera(java.lang.String)}.
+	 */
+	@Test
+	public void testRecupera() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link mx.uam.ayd.proyecto.datos.DAOArticuloEnAlmacenBD#actualiza(mx.uam.ayd.proyecto.negocio.dominio.ArticuloEnAlmacen)}.
+	 */
+	@Test
+	public void testActualiza() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link mx.uam.ayd.proyecto.datos.DAOArticuloEnAlmacenBD#borra(mx.uam.ayd.proyecto.negocio.dominio.ArticuloEnAlmacen)}.
+	 */
+	@Test
+	public void testBorra() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link mx.uam.ayd.proyecto.datos.DAOArticuloEnAlmacenBD#recuperaTodos()}.
+	 */
+	@Test
+	public void testRecuperaTodos() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link mx.uam.ayd.proyecto.datos.DAOArticuloEnAlmacenBD#recuperaLapso(java.sql.Date, java.sql.Date)}.
+	 */
+	@Test
+	public void testRecuperaLapso() {
+		fail("Not yet implemented");
+	}
+
+}
--- ProyectoAYDAdminVic/src/test/mx/uam/ayd/proyecto/datos/DAOLibroBDTest.java
+++ ProyectoAYDAdminVic/src/test/mx/uam/ayd/proyecto/datos/DAOLibroBDTest.java
@@ -2,6 +2,7 @@ package test.mx.uam.ayd.proyecto.datos;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
